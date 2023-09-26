e61f892d6834c4639dd4d7af7ec4adf3aba00157
Ahmad Sufiyan <sufiyan.ae@gmail.com>
2023-09-26 15:49:44 +0700

    Initial code

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="page">
+      <section className="py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+            <div className="card">
+            <div className="card-header">
+              
+            <h1 className="text-center">Halo Github Page</h1>
+            </div>
+              <div className="card-body">
+                <p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Necessitatibus rem nisi vitae nesciunt laboriosam ullam nihil repellat itaque quo hic quibusdam et at, nam illo explicabo officiis saepe accusamus, reprehenderit.</p>
+              </div>
+            </div>
+
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
